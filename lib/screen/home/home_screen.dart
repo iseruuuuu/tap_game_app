37a6bed9db77744f1a3053baf06af6032d6d5a88
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-11-13 18:44:32 +0900

    add: タイトル画面のUIを作成

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tap_game_app/screen/home/home_screen_controller.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,21 +10,70 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(HomeScreenController());
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        elevation: 0,
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            TextButton(
-              onPressed: controller.onTapGameStart,
+            NeumorphicText(
+              '連打ゲーム',
+              textStyle: NeumorphicTextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            NeumorphicText(
+              'Mash Game',
+              textStyle: NeumorphicTextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            NeumorphicButton(
+              style: const NeumorphicStyle(
+                depth: 0,
+                color: Colors.red,
+              ),
+              onPressed: () {},
               child: const Text(
-                'レベル１',
+                'Revel１',
                 style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 30,
+                  color: Colors.white,
                 ),
               ),
             ),
+            const SizedBox(height: 20),
+            NeumorphicButton(
+              style: const NeumorphicStyle(
+                depth: -20,
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Revel 2',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            NeumorphicButton(
+              style: const NeumorphicStyle(
+                depth: -20,
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Revel 3',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Spacer(),
           ],
         ),
       ),
