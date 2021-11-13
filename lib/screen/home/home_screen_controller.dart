3c069024083127101766e44785ea50b1ca36d5ae
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-11-13 21:36:16 +0900

    add: 難易度を6個にした

--- lib/screen/home/home_screen_controller.dart
+++ lib/screen/home/home_screen_controller.dart
@@ -6,6 +6,9 @@ class HomeScreenController extends GetxController {
   var level1 = true.obs;
   var level2 = false.obs;
   var level3 = false.obs;
+  var level4 = false.obs;
+  var level5 = false.obs;
+  var level999 = false.obs;
 
   @override
   void onInit() {
@@ -14,11 +17,11 @@ class HomeScreenController extends GetxController {
   }
 
   void sharedPreference() async {
-    var stage2 = await Preference().getBool(PreferenceKey.level2);
-    level2.value = stage2;
-
+    level2.value = await Preference().getBool(PreferenceKey.level2);
     level3.value = await Preference().getBool(PreferenceKey.level3);
-
+    level4.value = await Preference().getBool(PreferenceKey.level4);
+    level5.value = await Preference().getBool(PreferenceKey.level5);
+    level999.value = await Preference().getBool(PreferenceKey.level999);
   }
 
   onTapGameStart() {
