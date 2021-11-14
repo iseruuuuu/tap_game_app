37179d58e24e341c372a5975c2b4ece51ac391e6
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-11-14 16:05:11 +0900

    fix: sort indent

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(HomeScreenController());
+    final controller = Get.put(HomeScreenController(),tag: 'ああ');
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
@@ -43,49 +43,54 @@ class HomeScreen extends StatelessWidget {
               colors: controller.level1.value ? Colors.red : Colors.grey,
             ),
             const SizedBox(height: 20),
-            TitleButton(
-              depth: controller.level2.value ? 0 : -20,
-              title: 'Level2',
-              onTap: () => controller.level2.value
-                  ? controller.onTapGameStart(2, 70, 'い')
-                  : null,
-              colors: controller.level2.value ? Colors.red : Colors.grey,
+            Obx(() => TitleButton(
+                depth: controller.level2.value ? 0 : -20,
+                title: 'Level2',
+                onTap: () => controller.level2.value
+                    ? controller.onTapGameStart(2, 70, 'い')
+                    : null,
+                colors: controller.level2.value ? Colors.red : Colors.grey,
+              ),
             ),
             const SizedBox(height: 20),
-            TitleButton(
-              depth: controller.level3.value ? 0 : -20,
-              title: 'Level3',
-              onTap: () => controller.level3.value
-                  ? controller.onTapGameStart(3, 90, 'う')
-                  : null,
-              colors: controller.level3.value ? Colors.red : Colors.grey,
+            Obx(() => TitleButton(
+                depth: controller.level3.value ? 0 : -20,
+                title: 'Level3',
+                onTap: () => controller.level3.value
+                    ? controller.onTapGameStart(3, 90, 'う')
+                    : null,
+                colors: controller.level3.value ? Colors.red : Colors.grey,
+              ),
             ),
             const SizedBox(height: 20),
-            TitleButton(
-              depth: controller.level4.value ? 0 : -20,
-              title: 'Level4',
-              onTap: () => controller.level4.value
-                  ? controller.onTapGameStart(4, 110, 'え')
-                  : null,
-              colors: controller.level4.value ? Colors.red : Colors.grey,
+            Obx(() => TitleButton(
+                depth: controller.level4.value ? 0 : -20,
+                title: 'Level4',
+                onTap: () => controller.level4.value
+                    ? controller.onTapGameStart(4, 110, 'え')
+                    : null,
+                colors: controller.level4.value ? Colors.red : Colors.grey,
+              ),
             ),
             const SizedBox(height: 20),
-            TitleButton(
-              depth: controller.level5.value ? 0 : -20,
-              title: 'Level5',
-              onTap: () => controller.level5.value
-                  ? controller.onTapGameStart(5, 130, 'お')
-                  : null,
-              colors: controller.level5.value ? Colors.red : Colors.grey,
+            Obx(() => TitleButton(
+                depth: controller.level5.value ? 0 : -20,
+                title: 'Level5',
+                onTap: () => controller.level5.value
+                    ? controller.onTapGameStart(5, 130, 'お')
+                    : null,
+                colors: controller.level5.value ? Colors.red : Colors.grey,
+              ),
             ),
             const SizedBox(height: 20),
-            TitleButton(
-              depth: controller.level999.value ? 0 : -20,
-              title: 'Level999',
-              onTap: () => controller.level999.value
-                  ? controller.onTapGameStart(999, 200, 'か')
-                  : null,
-              colors: controller.level999.value ? Colors.red : Colors.grey,
+            Obx(() => TitleButton(
+                depth: controller.level999.value ? 0 : -20,
+                title: 'Level999',
+                onTap: () => controller.level999.value
+                    ? controller.onTapGameStart(999, 200, 'か')
+                    : null,
+                colors: controller.level999.value ? Colors.red : Colors.grey,
+              ),
             ),
             const SizedBox(height: 20),
             const Spacer(),
