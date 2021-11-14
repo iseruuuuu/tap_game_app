c6c612091d4db62d10b501cad6d2d64200e2fd20
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-11-14 11:57:35 +0900

    add: 値を渡せるようにするs

--- lib/preference/share_preferenced.dart
+++ lib/preference/share_preferenced.dart
@@ -19,7 +19,7 @@ class Preference {
 
   Future<bool> getBool(PreferenceKey key) async {
     final pref = await preference;
-    final value = pref.getBool(EnumToString.convertToString(key)) ?? true;
+    final value = pref.getBool(EnumToString.convertToString(key)) ?? false;
     return value;
   }
 
