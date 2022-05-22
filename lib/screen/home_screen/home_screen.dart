3d467d24a1bffb049aa896f1a7d5d64f432fbe83
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-22 09:30:56 +0900

    add: home画面をタイトル風にしてみた

---
+++ lib/screen/home_screen/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:tap_game_app/screen/home_screen/children/home_button.dart';
+import 'package:tap_game_app/screen/home_screen/home_screen_controller.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeScreenController(), tag: '');
+    return Scaffold(
+      backgroundColor: CupertinoColors.secondarySystemGroupedBackground,
+      appBar: AppBar(
+        backgroundColor: CupertinoColors.secondarySystemGroupedBackground,
+        elevation: 0,
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: controller.sharedPreference,
+          child: Column(
+            children: [
+              const Text(
+                '連打ゲーム',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              HomeButton(
+                title: '30秒連打モード',
+                onTap: controller.onTap30,
+                colors: Colors.blueAccent,
+                depth: 0,
+              ),
+              const Spacer(),
+              HomeButton(
+                title: 'チャレンジモード',
+                onTap: controller.onTapChallenge,
+                colors: Colors.blueAccent,
+                depth: 0,
+              ),
+              const Spacer(),
+              HomeButton(
+                title: '設定',
+                onTap: controller.onTapSetting,
+                colors: Colors.blueAccent,
+                depth: 0,
+              ),
+              const SizedBox(height: 20),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/home_screen/home_screen_controller.dart
+++ lib/screen/home_screen/home_screen_controller.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
-import 'package:tap_game_app/screen/game/game_screen.dart';
 import 'package:tap_game_app/preference/share_preferenced.dart';
+import 'package:tap_game_app/screen/challenge_list_screen/challenge_list_screen.dart';
+import 'package:tap_game_app/screen/game_30_screen/game_30_screen.dart';
+import 'package:tap_game_app/screen/setting_screen/setting_screen.dart';
 
 class HomeScreenController extends GetxController {
   var level1 = true.obs;
@@ -16,15 +18,24 @@ class HomeScreenController extends GetxController {
     sharedPreference();
   }
 
+  void onTap30() {
+    //TODO 後で記載が必要
+    Get.to(() => const Game30Screen(level: 0, keyword: ''));
+  }
+
+  void onTapChallenge() {
+    Get.to(() => const ChallengeListScreen());
+  }
+
+  void onTapSetting() {
+    Get.to(() => const SettingScreen());
+  }
+
   void sharedPreference() async {
     level2.value = await Preference().getBool(PreferenceKey.level2);
     level3.value = await Preference().getBool(PreferenceKey.level3);
     level4.value = await Preference().getBool(PreferenceKey.level4);
     level5.value = await Preference().getBool(PreferenceKey.level5);
     level999.value = await Preference().getBool(PreferenceKey.level999);
-  }
-
-  onTapGameStart(int level, int count, String keyword) {
-    Get.to(() => GameScreen(level: level, keyword: keyword));
   }
 }
