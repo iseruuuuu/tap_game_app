fbc5e001a6b9e69ffa2a86dba4abf0ff1da9dfc2
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-22 15:49:20 +0900

    add: ボタンを押すと大小するものを入れてみた

---
+++ lib/screen/game_30_screen/bounce_button.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+class BounceButton extends MaterialButton {
+  const BounceButton({
+    Key? key,
+    required VoidCallback onPressed,
+    VoidCallback? onLongPress,
+    ButtonTextTheme? textTheme,
+    required Color textColor,
+    Color? disabledTextColor,
+    required Color color,
+    Color? disabledColor,
+    Brightness? colorBrightness,
+    double elevation = 0,
+    EdgeInsetsGeometry? padding,
+    ShapeBorder? shape,
+    Clip clipBehavior = Clip.none,
+    FocusNode? focusNode,
+    bool autofocus = false,
+    MaterialTapTargetSize? materialTapTargetSize,
+    Duration animationDuration = kThemeChangeDuration,
+    required Widget child,
+    this.ratio = 0.95,
+  })  : assert(autofocus != null),
+        assert(clipBehavior != null),
+        super(
+          key: key,
+          onPressed: onPressed,
+          onLongPress: onLongPress,
+          textTheme: textTheme,
+          textColor: textColor,
+          disabledTextColor: disabledTextColor,
+          color: color,
+          disabledColor: disabledColor,
+          colorBrightness: colorBrightness,
+          elevation: elevation,
+          padding: padding,
+          shape: shape,
+          clipBehavior: clipBehavior,
+          focusNode: focusNode,
+          autofocus: autofocus,
+          materialTapTargetSize: materialTapTargetSize,
+          animationDuration: animationDuration,
+          child: child,
+        );
+
+  final double ratio;
+
+  @override
+  Widget build(BuildContext context) {
+    return BounceAnimation(
+      onPressed: () => onPressed,
+      onLongPress: () => onLongPress,
+      ratio: ratio,
+      animationDuration: animationDuration!,
+      child: AbsorbPointer(
+        // stops propagation of tap events.
+        child: super.build(context),
+      ),
+    );
+  }
+}
+
+class BounceAnimation extends StatefulWidget {
+  const BounceAnimation({
+    required this.onPressed,
+    this.onLongPress,
+    required this.child,
+    this.ratio = 0.95,
+    this.animationDuration = kThemeChangeDuration,
+    this.scaleAlignment = Alignment.center,
+  });
+
+  final VoidCallback onPressed;
+  final VoidCallback? onLongPress;
+  final Widget child;
+  final double ratio;
+  final Duration animationDuration;
+  final Alignment scaleAlignment;
+
+  bool get enabled => onPressed != null || onLongPress != null;
+
+  @override
+  _BounceAnimationState createState() => _BounceAnimationState();
+}
+
+class _BounceAnimationState extends State<BounceAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scale;
+  late TickerFuture _tickerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.animationDuration,
+    );
+    _scale = _controller
+        .drive(
+          CurveTween(curve: Curves.linear),
+        )
+        .drive(
+          Tween(begin: 1, end: widget.ratio),
+        );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _forwardAnimation() {
+    if (!widget.enabled) {
+      return;
+    }
+
+    _tickerFuture = _controller.forward();
+  }
+
+  void _reverseAnimation() {
+    if (!widget.enabled) {
+      return;
+    }
+
+    // start the reverse animation after the forward animation ends.
+    _tickerFuture.whenCompleteOrCancel(() {
+      _controller.reverse();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => _forwardAnimation(),
+      onTapUp: (_) => _reverseAnimation(),
+      onTapCancel: _reverseAnimation,
+      onTap: widget.onPressed,
+      onLongPress: widget.onLongPress,
+      child: ScaleTransition(
+        scale: _scale,
+        alignment: widget.scaleAlignment,
+        child: widget.child,
+      ),
+    );
+  }
+}
