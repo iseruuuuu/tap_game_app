50a0dd7387602b9ab605fb57ed9b13ec73cb0b34
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-05-24 08:34:30 +0900

    add: Preferenceを追加した

--- lib/screen/challenge_list_screen/challenge_list_screen_controller.dart
+++ lib/screen/challenge_list_screen/challenge_list_screen_controller.dart
@@ -1,9 +1,9 @@
 // Flutter imports:
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:get/get.dart';
+import 'package:tap_game_app/preference/share_preferenced.dart';
 import 'package:tap_game_app/screen/game_screen/game_screen.dart';
 
 // Project imports:
@@ -17,13 +17,36 @@ class ChallengeScreenController extends GetxController
   var level5 = false.obs;
   var level999 = false.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    getPreference();
+  }
+
+  void getPreference() async {
+    level2.value = await Preference().getBool(PreferenceKey.level2);
+    level3.value = await Preference().getBool(PreferenceKey.level3);
+    level4.value = await Preference().getBool(PreferenceKey.level4);
+    level5.value = await Preference().getBool(PreferenceKey.level5);
+    level999.value = await Preference().getBool(PreferenceKey.level999);
+  }
+
   void onTapBack() {
     Get.back();
   }
 
-
-
-  onTapGameStart(int level, int count, String keyword) {
-    Get.to(() => GameScreen(level: level, keyword: keyword));
+  onTapGameStart({
+    required int level,
+    required int count,
+    required String keyword,
+    required PreferenceKey preferenceKey,
+  }) {
+    Get.to(
+      () => GameScreen(
+        level: level,
+        keyword: keyword,
+        preferenceKey: preferenceKey,
+      ),
+    );
   }
 }
